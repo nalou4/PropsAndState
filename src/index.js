9c0b81f287336a199296b02182b402eec2ee9bfd
Nikki Lou <nalou4@gmail.com>
2023-04-28 05:27:22 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 // write your Color component here
 
 const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState('no color selected');
+  const selectColor = (colorName, string) => {
+    setSelectedColor(colorName + string);
+  }
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red" selectColor={selectColor} setSelectedColor={setSelectedColor} />
+        <Color color="blue" selectColor={selectColor} setSelectedColor={setSelectedColor} />
+        <Color color="green" selectColor={selectColor} setSelectedColor={setSelectedColor} />
+      </div>
     </div>
-  );
+  )
+}
+
+const Color = ({ color, selectColor, setSelectedColor }) => {
+  const border = ' selected';
+  return (
+    <div className={color} onClick={(e) => {
+      selectColor(color, border);
+      let selected = document.querySelectorAll('.selected')
+      if (selected) {
+        const allElements = document.querySelectorAll('.selected');
+        allElements.forEach((element) => {
+          console.log(element);
+          element.classList.remove('selected');
+        });
+      }
+      e.currentTarget.classList.add('selected')
+    }} />
+  )
 }
 
 ReactDOM.render(
